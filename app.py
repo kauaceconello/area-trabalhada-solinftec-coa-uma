752c4835e91fbc2dbfe80fc9503c02534f7c9234
Kauã Ceconello <154555841+kauaceconello@users.noreply.github.com>
2026-04-10 10:21:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ if uploaded_zip and uploaded_gpkg and GERAR:
                             centroid.x,
                             centroid.y,
                             str(row["TALHAO"]),
-                            fontsize=5,
+                            fontsize=7,
                             ha="center",
                             va="center",
                             color="black",
